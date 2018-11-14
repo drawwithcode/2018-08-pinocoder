0f38c9465a8b15bb8713e7a6aa3fb344b866c756
pinocoder <41514345+pinocoder@users.noreply.github.com>
2018-11-14 10:42:49 +0100

    Esercitazione in aula

--- sketch.js
+++ sketch.js
@@ -4,8 +4,18 @@ function preload(){
 
 function setup() {
   // put setup code here
+  rectMode(CENTER);
+  noFill();
+  stroke(25);
 }
 
 function draw() {
   // put drawing code here
+  translate(width/2, height/2);
+  background(120);
+  rect(width/2, heigh/2, 300, 300);
+}
+
+function mousePressed() {
+  if (mouseX)
 }
